6a94d095958ec44c8eac906540a8c18d0dc488ca
shevadeakash <shevadeakash4@gmail.com>
2025-02-11 10:02:54 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
-num1=int(input("Enter any number")
-num2=int(input("Enter another number")
-
-c=num1+num2
+num1=int(input("Enter any number"))
+num2=int(input("Enter another number"))
+num3=int(input("Enter third number"))
+c=num1+num2+num3
 print("Addition of numbers",c)
 
